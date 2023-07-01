b0eb839fbb354615bdd2a47edfeacee431592276
Pavan Macharla <85890242+PavanMacharla@users.noreply.github.com>
2023-07-02 01:11:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const authRoutes = require("./routes/auth");
-// const messageRoutes = require("./routes/messages");
+// const express = require("express");
+// // const cors = require("cors");
+// // const mongoose = require("mongoose");
+// // const authRoutes = require("./routes/auth");
+// // const messageRoutes = require("./routes/messages");
 const app = express();
-// const socket = require("socket.io");
-const PORT = 4000
-// app.use(cors());
-// app.use(express.json());
+const socket = require("socket.io");
+const PORT = 5000
+app.use(cors());
+app.use(express.json());
 
 // mongoose
 //   .connect("mongodb+srv://pavan:pavan@cluster0.ushqo.mongodb.net/?retryWrites=true&w=majority", {
@@ -23,30 +23,30 @@ const PORT = 4000
 
 // app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoutes);
- app.listen(PORT, () =>
+ const server = app.listen(PORT, () =>
   console.log(`Server started on 5000`)
 );
-// const io = socket(server, {
-//   cors: {
-//     origin: "https://chattingapp-rayo.vercel.app/",
-//     credentials: true,
-//   },
-// });
+const io = socket(server, {
+  cors: {
+    origin: "https://chattingapp-rayo.vercel.app/",
+    credentials: true,
+  },
+});
 
-// global.onlineUsers = new Map();
-// io.on("connection", (socket) => {
-//   global.chatSocket = socket;
-//   socket.on("add-user", (userId) => {
-//     onlineUsers.set(userId, socket.id);
-//   });
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
-//   socket.on("send-msg", (data) => {
-//     const sendUserSocket = onlineUsers.get(data.to);
-//     if (sendUserSocket) {
-//       socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-//     }
-//   });
-// });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
 
 // const express = require('express')
 
